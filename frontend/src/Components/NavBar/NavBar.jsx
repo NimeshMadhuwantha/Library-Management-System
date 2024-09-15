af637150e0cca9a226f56b353b1156a2c01fd4b5
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-09-15 19:31:59 +0530

    finish and responsive the navbar

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -13,19 +13,28 @@ const NavBar = () => {
     <div>
       <nav className="navbar">
         <div className="navbar-logo">BookIt</div>
+
         <ul className={`navbar-links ${isMenuOpen ? 'active' : ''}`}>
           <li><a href="/">Home</a></li>
           <li><a href="/books">Books</a></li>
           <li><a href="/categories">Categories</a></li>
           <li><a href="/contact">Contact Us</a></li>
+
+          <div className="navbar-small-screen-buttons">
+            <button className="btn login-btn">Login</button>
+            <button className="btn signup-btn">Sign Up</button>
+          </div>
+
           <div className="navbar-close-icon" onClick={toggleMenu}>
             <FaTimes /> 
           </div>
         </ul>
+
         <div className="navbar-buttons">
           <button className="btn login-btn">Login</button>
           <button className="btn signup-btn">Sign Up</button>
         </div>
+
         <div className="navbar-menu-icon" onClick={toggleMenu}>
           <FaBars /> 
         </div>
