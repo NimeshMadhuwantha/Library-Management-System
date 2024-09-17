4223c68ca3033920569bf8cd9e56ed61549672a2
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-09-17 20:46:40 +0530

    create category book section

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import './Home.css';
 import LibraryHome from '../../assets/LibraryHome.jpg'
 import { FaSearch } from 'react-icons/fa';
+import Book1 from '../../assets/Books/Book1.jpeg'
+import Book2 from '../../assets/Books/Book2.jpeg'
+import Book3 from '../../assets/Books/Book3.jpeg'
+import Book4 from '../../assets/Books/Book4.jpeg'
+
 
 const Home = () => {
   return (
@@ -15,6 +20,10 @@ const Home = () => {
         <div className="background-overlay">
           <h1>BookIT Biggest Library</h1>
           <p>Your ultimate destination for endless knowledge and adventure.</p>
+          <div className="Home-Screen-buttons">
+            <button className="ViewBook-btn">View Books</button>
+            <button className="AddBook-btn">Add Books</button>
+          </div>
         </div>
 
        
@@ -30,8 +39,10 @@ const Home = () => {
       <div className="form-section">
         <input type="text" placeholder="Author Name" />
         <input type="text" placeholder="Book Name" />
+        <input type="number" placeholder="Year" min="1000" max="9999" /> 
         <button>Search</button>
       </div>
+
 
       {/* Welcome */}
       <div className="welcome-section">
@@ -46,6 +57,36 @@ const Home = () => {
         <div className="category-card">Adult</div>
         <div className="category-card">Funny</div>
       </div>
+
+      <div className="top-categories-section">
+        <h2>Our Top Categories</h2>
+        <p>Explore our curated selection of top books across various genres.</p>
+        
+        <div className="top-categories-cards">
+          <div className="category-Book">
+            <img src={Book1} alt="Book 1" />
+            <div className="card-content">
+            </div>
+          </div>
+          <div className="category-Book">
+            <img src={Book2} alt="Book 2" />
+            <div className="card-content">
+            </div>
+          </div>
+          <div className="category-Book">
+            <img src={Book3} alt="Book 3" />
+            <div className="card-content">
+            </div>
+          </div>
+          <div className="category-Book">
+            <img src={Book4} alt="Book 4" />
+            <div className="card-content">
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   )
 }
